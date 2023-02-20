6e8dc26ae0fee1f240bef1ff8564520fdd33f029
Rajat Ghosh <rajatghosh@Rajats-MacBook-Pro.local>
2023-02-20 17:56:54 +0100

    first commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import aiImageRoutes from './routes/aiImageRoutes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/aiImage', aiImageRoutes);
+
+app.get('/', async (req, res) => {
+  res.send('Hello from AI Image');
+});
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => {
+      console.log('Server has started on port http://localhost:8080');
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
